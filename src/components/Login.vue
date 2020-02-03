32b9573009a471389497487fa86b9dac425bbda5
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2020-02-03 10:14:13 +0100

    added login and home

---
+++ src/components/Login.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="about">
+    <h2>Login</h2>
+    <div class="about-container">
+      <section>
+        <form @submit.prevent="submitForm">
+          <label for="email"></label>
+          <input type="text" v-model="about.email" />
+          <label for="password"></label>
+          <input type="text" v-model="about.password" />
+          <button v-on:click="submitForm" class="btnPrimary" type="submit">
+            Save
+          </button>
+          <div class="errors" v-if="errors.length">
+            <p>{{ errors }}</p>
+          </div>
+        </form>
+      </section>
+    </div>
+    <button v-on:click="showClickEditAbout" class="btnPrimary">Edit</button>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+const apiKey = process.env.VUE_APP_API_KEY
+
+export default {
+  data() {
+    return {
+      data: {},
+      errors: []
+    }
+  },
+  props: {
+    about: {
+      type: Object
+    }
+  },
+
+  methods: {
+    showClickEditAbout: function(e) {},
+    submitForm() {
+      axios
+        .put(
+          `http://localhost:1337`,
+          {
+            name: this.about.name,
+            description: this.about.description,
+            homeTown: this.about.location,
+            interest: this.about.interest
+          },
+          {
+            headers: {
+              api_key: apiKey,
+              'Content-type': 'application/json',
+              'x-access-token': localStorage.token
+            }
+          }
+        )
+        .then(response => {
+          // JSON responses are automatically parsed.
+          console.log(response)
+        })
+        .catch(e => {
+          console.log('====================================')
+          console.log(e.response.data)
+          console.log('====================================')
+          this.errors.push(e.response.data.response)
+        })
+    }
+  }
+
+  // Fetches posts when the component is created.
+  // created() {
+  //
+  // }
+}
+</script>
---
+++ src/views/Skills.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <Skills msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Skills from '@/components/Skills.vue'
+
+export default {
+  name: 'skills',
+  components: {
+    Skills
+  }
+}
+</script>
